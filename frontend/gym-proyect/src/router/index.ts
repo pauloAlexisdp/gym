23e26a8879012805b9b1484b948e4910cf68de5c
Paulo Alexis Parraguez Diaz <44139011+pauloAlexisdp@users.noreply.github.com>
2025-10-04 20:25:07 -0300

    feat(front): Update auth routes

--- frontend/gym-proyect/src/router/index.ts
+++ frontend/gym-proyect/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
@@ -10,20 +10,12 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/login',
+      path: '/iniciar-sesion',
       name: 'login',
       component: () => import('../views/LoginView.vue'),
     },
     {
-      path: '/register',
+      path: '/registrarse',
       name: 'register',
       component: () => import('../views/RegisterView.vue'),
     },
